2af072481755ecd334a54f72eef0d73ff3a9e233
w981230w <w981230w@gmail.com>
2024-07-24 02:18:46 +0900

    NPC_HeadTrigger 추가
    NPC 머리 위에 있을 때 트리거로 레이어 값 변경

---
+++ Assets/NPC_HeadLayer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_HeadLayer : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spr;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        spr.sortingOrder = 1;
+        Debug.Log("들어옴");
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        spr.sortingOrder = -1;
+        Debug.Log("나감");
+    }
+}
