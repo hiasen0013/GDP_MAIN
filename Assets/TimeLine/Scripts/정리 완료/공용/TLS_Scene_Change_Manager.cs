fbf723b4cbd24d3aa86b512a010c5d9fbc41bbcf
w981230w <w981230w@gmail.com>
2024-07-28 19:45:53 +0900

    타임라인 씬 이동
    ㅇㅇ

--- Assets/TimeLine/Scripts/정리 완료/공용/TLS_Scene_Change_Manager.cs
+++ Assets/TimeLine/Scripts/정리 완료/공용/TLS_Scene_Change_Manager.cs
@@ -11,10 +11,24 @@ public class TLS_Scene_Change_Manager : MonoBehaviour
         SceneManager.LoadScene("1_Room");
     }
 
+    public void End_Second_TLS()
+    {
+        if(SystemMessage.instance.YorN)
+        {
+            SceneManager.LoadScene("3_엘레베이터 안");
+        }
+        else if(!SystemMessage.instance.YorN)
+        {
+            SceneManager.LoadScene("3_엘레베이터 안");
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player"))
         {
             SceneManager.LoadScene("2_엘레베이터 앞");
         }
     }
+    
+
 }
