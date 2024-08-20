0331b7401162db82709463ee8d955fa7677612bc
hiasen0013 <87457023+hiasen0013@users.noreply.github.com>
2024-08-20 19:27:44 +0900

    수정

--- Assets/Script/맵 이동/MoveTrigger.cs
+++ Assets/Script/맵 이동/MoveTrigger.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
+
 public class MoveTrigger : MonoBehaviour
 {
+    private FadeEffect fadeEffect;
+
     protected virtual void Start()
     {
+        // FadeEffect 컴포넌트를 찾아 저장합니다.
+        fadeEffect = FindObjectOfType<FadeEffect>();
+
         foreach (Transform child in transform)
         {
             ChildTrigger childTrigger = child.gameObject.AddComponent<ChildTrigger>();
@@ -16,14 +22,23 @@ public class MoveTrigger : MonoBehaviour
         Transform currentChildTransform = transform.GetChild(childId);
         Debug.Log($"{childId} 자식 오브젝트에서 {other.gameObject.name}이(가) 트리거에 들어왔습니다.");
         Debug.Log($"자식 오브젝트의 위치: {currentChildTransform.position}");
+
+        // 트리거 발생 시 화면이 밝아지도록 FadeEffect의 Onfade 메서드 호출
+        if (fadeEffect != null)
+        {
+            fadeEffect.Onfade(FadeState.FadeIn);
+        }
+        else
+        {
+            Debug.LogWarning("FadeEffect 컴포넌트를 찾을 수 없습니다.");
+        }
     }
 }
 
 public class ChildTrigger : MonoBehaviour
 {
     private MoveTrigger moveTrigger;
-    [SerializeField]private int childId;
-
+    [SerializeField] private int childId;
 
     public void Setup(MoveTrigger moveTrigger, int childId)
     {
