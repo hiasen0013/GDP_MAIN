01c95ffe5f918465d96fe670929444411e858ebb
w981230w <w981230w@gmail.com>
2024-09-15 11:51:51 +0900

    지하3층 간격 수정 + 화장실 추가 + 스크립트 수정
    ㅇㅇ

--- Assets/Script/맵 이동/B3F_MoveTrigger.cs
+++ Assets/Script/맵 이동/B3F_MoveTrigger.cs
@@ -15,8 +15,6 @@ public class B3F_MoveTrigger : MoveTrigger
     protected override void Start()
     {
         base.Start();
-
-        /*
         if(b3_Lobby == null)
         {
             b3_Lobby = GameObject.Find("B3_로비");
@@ -44,13 +42,7 @@ public class B3F_MoveTrigger : MoveTrigger
         if(b3_Warehouse == null)
         {
             b3_Warehouse = GameObject.Find("B3_창고");
-        } */
-        b3_Lobby.SetActive(true); 
-        b3_Library.SetActive(false); 
-        b3_Corridor.SetActive(false); 
-        b3_Cafeteria.SetActive(false); 
-        b3_CounselingRoom.SetActive(false);
-        b3_Warehouse.SetActive(false);
+        }
     }
 
     public override void OnChildTriggerEnter(Collider2D other, int childId)
@@ -62,8 +54,6 @@ public class B3F_MoveTrigger : MoveTrigger
                 Vector2 newPosition0 = new Vector2(nextChildTransform0.position.x, nextChildTransform0.position.y - 2f);
                 other.transform.position = newPosition0;
                 Debug.Log($"{childId}번째 자식 오브젝트와 충돌하여 이동하였습니다.");
-                b3_CounselingRoom.SetActive(false);
-                b3_Corridor.SetActive(true);
                 break;
             case 1: // 복도에서 상담실로
                 
@@ -71,8 +61,6 @@ public class B3F_MoveTrigger : MoveTrigger
                 Vector2 newPosition1 = new Vector2(nextChildTransform1.position.x, nextChildTransform1.position.y + 2f);
                 other.transform.position = newPosition1;
                 Debug.Log($"{childId}번째 자식 오브젝트와 충돌하여 이동하였습니다.");
-                b3_Corridor.SetActive(false);
-                b3_CounselingRoom.SetActive(true);
                 if(GameProgress.instance.tlProgress.prolog0_3)
                 {
                     SceneManager.LoadScene("0-5_지하3층_상담실");
@@ -85,14 +73,13 @@ public class B3F_MoveTrigger : MoveTrigger
                 Vector2 newPosition2 = new Vector2(nextChildTransform2.position.x, nextChildTransform2.position.y - 2f);
                 other.transform.position = newPosition2;
                 Debug.Log($"{childId}번째 자식 오브젝트와 충돌하여 이동하였습니다.");
-                b3_Corridor.SetActive(true);
                 break;
+
             case 3: // 복도에서 재활치료실로
                 Transform nextChildTransform3 = transform.GetChild(2);
                 Vector2 newPosition3 = new Vector2(nextChildTransform3.position.x, nextChildTransform3.position.y + 2f);
                 other.transform.position = newPosition3;
                 Debug.Log($"{childId}번째 자식 오브젝트와 충돌하여 이동하였습니다.");
-                b3_Corridor.SetActive(false);
                 break;
 
 
@@ -101,16 +88,12 @@ public class B3F_MoveTrigger : MoveTrigger
                 Vector2 newPosition4 = new Vector2(nextChildTransform4.position.x + 2f, nextChildTransform4.position.y);
                 other.transform.position = newPosition4;
                 Debug.Log($"{childId}번째 자식 오브젝트와 충돌하여 이동하였습니다.");
-                b3_Corridor.SetActive(false);
-                b3_Lobby.SetActive(true);
                 break;
             case 5: // 로비에서 복도로
                 Transform nextChildTransform5 = transform.GetChild(4);
                 Vector2 newPosition5 = new Vector2(nextChildTransform5.position.x -2f, nextChildTransform5.position.y);
                 other.transform.position = newPosition5;
                 Debug.Log($"{childId}번째 자식 오브젝트와 충돌하여 이동하였습니다.");
-                b3_Lobby.SetActive(false);
-                b3_Corridor.SetActive(true);
                 break;
 
 
@@ -119,16 +102,12 @@ public class B3F_MoveTrigger : MoveTrigger
                 Vector2 newPosition6 = new Vector2(nextChildTransform6.position.x + 2f, nextChildTransform6.position.y);
                 other.transform.position = newPosition6;
                 Debug.Log($"{childId}번째 자식 오브젝트와 충돌하여 이동하였습니다.");
-                b3_Lobby.SetActive(false);
-                b3_Library.SetActive(true);
                 break;
             case 7: // 도서관에서 로비로
                 Transform nextChildTransform7 = transform.GetChild(6);
                 Vector2 newPosition7 = new Vector2(nextChildTransform7.position.x - 2f, nextChildTransform7.position.y);
                 other.transform.position = newPosition7;
                 Debug.Log($"{childId}번째 자식 오브젝트와 충돌하여 이동하였습니다.");
-                b3_Library.SetActive(false);
-                b3_Lobby.SetActive(true);
                 break;
 
 
@@ -137,16 +116,12 @@ public class B3F_MoveTrigger : MoveTrigger
                 Vector2 newPosition8 = new Vector2(nextChildTransform8.position.x, nextChildTransform8.position.y - 2f);
                 other.transform.position = newPosition8;
                 Debug.Log($"{childId}번째 자식 오브젝트와 충돌하여 이동하였습니다.");
-                b3_Corridor.SetActive(false);
-                b3_Cafeteria.SetActive(true);
                 break;
             case 9:
                 Transform nextChildTransform9 = transform.GetChild(8);
                 Vector2 newPosition9 = new Vector2(nextChildTransform9.position.x, nextChildTransform9.position.y + 2f);
                 other.transform.position = newPosition9;
                 Debug.Log($"{childId}번째 자식 오브젝트와 충돌하여 이동하였습니다.");
-                b3_Cafeteria.SetActive(false);
-                b3_Corridor.SetActive(true);
                 break;
 
 
@@ -155,8 +130,6 @@ public class B3F_MoveTrigger : MoveTrigger
                 Vector2 newPosition10 = new Vector2(nextChildTransform10.position.x - 2f, nextChildTransform10.position.y);
                 other.transform.position = newPosition10;
                 Debug.Log($"{childId}번째 자식 오브젝트와 충돌하여 이동하였습니다.");
-                b3_Library.SetActive(false);
-                b3_Warehouse.SetActive(true);
                 SpriteRenderer player_spr = other.gameObject.GetComponent<SpriteRenderer>();
                 Material spr = Resources.Load<Material>("Player_Materials/Sprite-Lit-Default");
                 player_spr.material = spr;
@@ -166,8 +139,6 @@ public class B3F_MoveTrigger : MoveTrigger
                 Vector2 newPosition11 = new Vector2(nextChildTransform11.position.x + 2f, nextChildTransform11.position.y);
                 other.transform.position = newPosition11;
                 Debug.Log($"{childId}번째 자식 오브젝트와 충돌하여 이동하였습니다.");
-                b3_Warehouse.SetActive(false);
-                b3_Library.SetActive(true);
                 player_spr = other.gameObject.GetComponent<SpriteRenderer>();
                 spr = Resources.Load<Material>("Player_Materials/Sprite-Unlit-Default");
                 player_spr.material = spr;
